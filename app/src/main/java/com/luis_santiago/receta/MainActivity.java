35318231bda9b0fc6160c5a86f41b99cdd7733fb
luissantiagodev <luissantiagodev@gmail.com>
2018-07-03 08:59:16 -0500

    setting adapter

--- app/src/main/java/com/luis_santiago/receta/MainActivity.java
+++ app/src/main/java/com/luis_santiago/receta/MainActivity.java
@@ -4,17 +4,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.LinearLayout;
+
+import com.luis_santiago.receta.POJOS.Receta;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    RecyclerView recycleview;
+    private RecyclerView recyclerRecetas;
+    private ArrayList<Receta> mRecetaList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        recycleview = findViewById(R.id.recycleview);
+        recyclerRecetas = findViewById(R.id.recycleview);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        recycleview.setLayoutManager(linearLayoutManager);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerRecetas.setLayoutManager(linearLayoutManager);
     }
+
+
+    public void createData(){
+
+    }
+
 }
---
+++ app/src/main/java/com/luis_santiago/receta/adapter/RecetaAdapter.java
@@ -0,0 +1,71 @@
+package com.luis_santiago.receta.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.luis_santiago.receta.POJOS.Receta;
+import com.luis_santiago.receta.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Luis Santiago on 7/2/18.
+ */
+public class RecetaAdapter extends RecyclerView.Adapter<RecetaAdapter.RecetaHolder>{
+
+    private ArrayList<Receta> mRecetaList;
+    private Context mContext;
+
+    public RecetaAdapter(ArrayList<Receta> recetas , Context context){
+        this.mRecetaList = recetas;
+        this.mContext = context;
+    }
+
+    @NonNull
+    @Override
+    public RecetaHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_receta , parent , false);
+        return new RecetaHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecetaHolder holder, int position) {
+        Receta currentReceta = mRecetaList.get(position);
+
+        holder.bindItem(currentReceta);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mRecetaList.size();
+    }
+
+    class RecetaHolder extends RecyclerView.ViewHolder{
+        private CardView mCardView;
+        private ImageView mImageView;
+        private TextView name, people, description;
+
+        RecetaHolder(View v) {
+            super(v);
+            mCardView = v.findViewById(R.id.card_view_receta);
+            mImageView = v.findViewById(R.id.image_view_receta);
+            name = v.findViewById(R.id.name);
+            people = v.findViewById(R.id.quantity_persons);
+            description = v.findViewById(R.id.description);
+        }
+
+        void bindItem(Receta currentReceta) {
+            name.setText(currentReceta.getNombre());
+            people.setText("Personas " + String.valueOf(currentReceta.getPersonas()));
+            description.setText(currentReceta.getDescripcion());
+        }
+    }
+}
